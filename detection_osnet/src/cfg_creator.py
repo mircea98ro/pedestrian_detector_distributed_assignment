d9c298f103d9c42996fc7dc8e456cd11baf1dc70
Mircea-Alexandru Popovici <mircea-alexandru.popovici@student.tuiasi.ro>
2022-06-16 13:44:26 +0200

    Yolo fix

--- detection_osnet/src/cfg_creator.py
+++ detection_osnet/src/cfg_creator.py
@@ -11,9 +11,9 @@ def cfg_creator():
     rospy.set_param('cfg/yolo/cfg', 'yolov3-tiny.cfg')
     rospy.set_param('cfg/yolo/names', 'coco.names')
 
-    rospy.set_param('cfg/yolo/max_iou', 0.4)
-    rospy.set_param('cfg/yolo/min_obj_confidence', 0.5)
-    rospy.set_param('cfg/yolo/min_score', 0.5)
+    rospy.set_param('cfg/yolo/max_iou', 0.8)
+    rospy.set_param('cfg/yolo/min_obj_confidence', 0.2)
+    rospy.set_param('cfg/yolo/min_score', 0.1)
 
     rospy.set_param('cfg/reid/path', os.path.join(os.path.dirname(__file__), '..', 'osnet_data'))
     rospy.set_param('cfg/reid/model', 'osnet_ain_x0_25')
@@ -23,13 +23,13 @@ def cfg_creator():
     rospy.set_param('cfg/reid/window_ratio', 2)
     rospy.set_param('cfg/reid/max_distance', 0.6)
     rospy.set_param('cfg/reid/redo_yolo_windows', 0)
-    rospy.set_param('cfg/reid/dynamic_gallery', 1)
-    rospy.set_param('cfg/reid/load_galleries', 1)
+    rospy.set_param('cfg/reid/dynamic_gallery', 0)
+    rospy.set_param('cfg/reid/load_galleries', 0)
 
     rospy.set_param('cfg/kalman/source', 'osnet')
 
     rospy.set_param('/master/robot_no', 1)
-    rospy.set_param('/master/target_no', 2)
+    rospy.set_param('/master/target_no', 6)
     rospy.set_param('/master/export', 0)
 
 #   Guard
